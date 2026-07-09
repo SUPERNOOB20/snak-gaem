e79468f4578f48e2bc6c364d466b27ba8a76f917
SUPERNOOB20 <andycia802@gmail.com>
2026-07-09 16:18:07 -0300

    RESIZABLE HITBOXES BABY LESGOOOO IT WROKS NOW (DO NOT LOOK AT CODE IN .H)

---
+++ 14/snak_gaem.h
@@ -0,0 +1,198 @@
+#ifndef SNAK_GAEM_H
+#define SNAK_GAEM_H
+
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+#include <string>
+#include <cassert>
+#include <tuple>
+
+
+
+// When you are coding in C/C++,
+// do you use #define or do you use const...?
+// 
+// Personally, I don't mind either way.
+// Let's use #define,
+// to be more explicit about our
+// preprocessor usage! ^_^
+
+#define WINDOW_WIDTH 64
+#define WINDOW_HEIGHT 36
+
+#define PLAYFIELD_WIDTH 48
+#define PLAYFIELD_HEIGHT 24
+
+double game_speed                  = 0.015;      // fine-tuned initial player speed value after a lot of testplaying... (MY own testplaying ":3)
+double chosen_game_speed_increment = 1.1;       //  fine-tuned initial player speed value after a lot of testplaying... (MY own testplaying ":3)
+
+
+
+namespace item
+{
+    std::tuple<Sint32, Sint32> generate_item(){
+
+        Sint32 item_x = SDL_rand(PLAYFIELD_WIDTH  - 2);
+        Sint32 item_y = SDL_rand(PLAYFIELD_HEIGHT - 2);
+        
+	    std::tuple<Sint32, Sint32> item_pos(item_x, item_y);
+            return item_pos;
+    }
+}
+
+
+namespace player_pos
+{
+
+	/*
+	// Tuple declaration.
+	std::tuple<int, int> current_pos;
+
+	// Assigning values to the tuple using make_tuple()
+	current_pos = std::make_tuple(100, 100);		// Current position of the player.
+	*/
+
+    // Start near the center of the screen idk
+	std::tuple<double, double> current_pos(WINDOW_WIDTH / 3.5, WINDOW_HEIGHT / 2.0);
+}
+
+
+SDL_FRect playfield{
+    .x = (WINDOW_WIDTH - PLAYFIELD_WIDTH) / 2,
+    .y = (WINDOW_HEIGHT - PLAYFIELD_HEIGHT) / 2,
+    .w = PLAYFIELD_WIDTH,
+    .h = PLAYFIELD_HEIGHT
+};
+
+float scale_factor = 0.9f;
+
+SDL_FRect playfield_but_bigger{
+    .x = (WINDOW_WIDTH - PLAYFIELD_WIDTH - scale_factor) / 2,
+    .y = (WINDOW_HEIGHT - PLAYFIELD_HEIGHT - scale_factor) / 2,
+    .w = PLAYFIELD_WIDTH * 1.0f + scale_factor,
+    .h = PLAYFIELD_HEIGHT * 1.0f + scale_factor
+};
+
+
+namespace main_menu
+{
+    SDL_FRect easyTextRect{
+        .x = WINDOW_WIDTH   / 2.75f,                    //  Eyeballed center of the screen...
+        .y = WINDOW_HEIGHT  / 6.2f,
+        .w = WINDOW_WIDTH   / 3.2f,
+        .h = WINDOW_HEIGHT  / 8.0f
+    };
+
+    SDL_FRect mediumTextRect{
+        .x = WINDOW_WIDTH   / 3.0f,                     //  Eyeballed center of the screen...
+        .y = WINDOW_HEIGHT  / 2.2f,
+        .w = WINDOW_WIDTH   / 3.2f,
+        .h = WINDOW_HEIGHT  / 8.0f
+    };
+
+    SDL_FRect hardTextRect{
+        .x = WINDOW_WIDTH   / 2.75f,                    //  Eyeballed center of the screen...
+        .y = WINDOW_HEIGHT  / 1.32f,
+        .w = WINDOW_WIDTH   / 3.2f,
+        .h = WINDOW_HEIGHT  / 8.0f
+    };
+
+
+
+
+    SDL_FRect easyButtonRect{
+        .x = WINDOW_WIDTH   / 3.5f,                     //  Eyeballed center of the screen...
+        .y = WINDOW_HEIGHT  * (0.9f / 6.0f),
+        .w = WINDOW_WIDTH   / 2.2f,
+        .h = WINDOW_HEIGHT  * (1.0f / 6.0f)
+    };
+
+    SDL_FRect mediumButtonRect{
+        .x = WINDOW_WIDTH   / 3.5f,        //  Eyeballed center of the screen...
+        .y = WINDOW_HEIGHT  * (2.65f / 6.0f),
+        .w = WINDOW_WIDTH   / 2.2f,
+        .h = WINDOW_HEIGHT  * (1.0f / 6.0f)
+    };
+
+    SDL_FRect hardButtonRect{
+        .x = WINDOW_WIDTH   / 3.5f,        //  Eyeballed center of the screen...
+        .y = WINDOW_HEIGHT  * (4.4f / 6.0f),
+        .w = WINDOW_WIDTH   / 2.2f,
+        .h = WINDOW_HEIGHT  * (1.0f / 6.0f)
+    };
+}
+
+
+// SCORE COUNTER
+SDL_FRect scoreTextRect{
+    .x = WINDOW_WIDTH  / 2.0f,
+    .y =                 0.0f,
+    .w = WINDOW_WIDTH  / 2.2f,
+    .h = WINDOW_HEIGHT / 5.0f,
+};
+
+// GAME OVER SIGN.
+SDL_FRect gameOverTextRect{
+    .x = WINDOW_WIDTH   / 3.2f,        //  Eyeballed center of the screen...
+    .y = WINDOW_HEIGHT  / 2.2f,       //   Eyeballed center of the screen...
+    .w = WINDOW_WIDTH   / 2.2f,
+    .h = WINDOW_HEIGHT  / 5.0f
+};
+
+// You have to be moving SOMEWHERE when you first start the game, no?
+int pre_input = 82;
+std::string facing = "right";
+
+
+
+
+// Mouse handling (choosing the difficulty)
+
+float mouse_pos_x = 0;
+float mouse_pos_y = 0;
+
+bool player_has_clicked = false;
+
+/*
+bool is_player_hovering_over_obj(SDL_FRect* &rect){
+    return ((mouse_pos_x >= rect->x && mouse_pos_x <= rect->x + rect->w) && (mouse_pos_y >= rect->y && mouse_pos_y <= rect->y + rect->h));           // (player is within horizontal bounds) && (player is within vertical bounds)
+}
+*/
+
+
+/*
+bool is_player_hovering_over_obj(float mouse_pos_x, float mouse_pos_y, SDL_FRect rect){
+
+    // SDL_GetWindowSize(SDL_Window mWindow);
+
+    // SDL_Log("mouse pos x: %f", mouse_pos_x);
+    // SDL_Log("mouse pos y: %f", mouse_pos_y);
+
+    // SDL_Log("%d", (mouse_pos_x >= rect.x && mouse_pos_x <= rect.x + rect.w) && (mouse_pos_y >= rect.y && mouse_pos_y <= rect.y + rect.h));
+    // SDL_Log("0 == 1: %d", 0 == 1);
+    return ((mouse_pos_x >= rect.x && mouse_pos_x <= rect.x + rect.w) && (mouse_pos_y >= rect.y && mouse_pos_y <= rect.y + rect.h));           // (player is within horizontal bounds) && (player is within vertical bounds)
+}
+*/
+
+
+bool is_player_hovering_over_obj(float mouse_pos_x, float mouse_pos_y, SDL_FRect rect, int current_window_width, int current_window_height){
+
+    return ((mouse_pos_x >= (rect.x * (current_window_width / WINDOW_WIDTH))) && (mouse_pos_x <= (rect.x * (current_window_width / WINDOW_WIDTH)) + (rect.w * (current_window_width / WINDOW_WIDTH))) && (mouse_pos_y >= (rect.y * (current_window_height / WINDOW_HEIGHT))) && (mouse_pos_y <= (rect.y * (current_window_height / WINDOW_HEIGHT)) + (rect.h * (current_window_height / WINDOW_HEIGHT))));           // (player is within horizontal bounds) && (player is within vertical bounds)
+}
+
+
+bool easy_is_clicked(float mouse_pos_x, float mouse_pos_y, int current_window_width, int current_window_height){
+    return player_has_clicked && is_player_hovering_over_obj(mouse_pos_x, mouse_pos_y, main_menu::easyButtonRect, current_window_width, current_window_height);
+}
+
+bool medium_is_clicked(float mouse_pos_x, float mouse_pos_y, int current_window_width, int current_window_height){
+    return player_has_clicked && is_player_hovering_over_obj(mouse_pos_x, mouse_pos_y, main_menu::mediumButtonRect, current_window_width, current_window_height);
+}
+
+bool hard_is_clicked(float mouse_pos_x, float mouse_pos_y, int current_window_width, int current_window_height){
+    return player_has_clicked && is_player_hovering_over_obj(mouse_pos_x, mouse_pos_y, main_menu::hardButtonRect, current_window_width, current_window_height);
+}
+
+
+#endif
